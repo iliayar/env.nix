1fd5366b2fe72c0716adad9d04d44539e824df15
iliayar <iliayar3@gmail.com>
2023-12-26 21:48:34 +0300

    feat proto: add google api linter

--- modules/langs/proto.nix
+++ modules/langs/proto.nix
@@ -3,5 +3,5 @@ with lib;
 let cfg = config.langs.protobuf;
 in {
   options = { langs.protobuf = { enable = mkEnableOption { default = false; }; }; };
-  config = mkIf cfg.enable { denv.packages = with pkgs; [ buf ]; };
+  config = mkIf cfg.enable { denv.packages = with pkgs; [ buf api-linter ]; };
 }
